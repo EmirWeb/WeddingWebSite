dd6c57148c0489f41c9d78cc1a34b19028e8b7f3
Emir <emir@xtremelabs.com>
2013-02-17 11:28:15 -0500

    EmirH: Added wedding party and directions

--- Home.php
+++ Home.php
@@ -3,6 +3,7 @@
 	DomManager::addCSS('CSS/Body.css');
 	DomManager::addCSS('CSS/Home.css');
 	DomManager::addCSS('CSS/Strips.css');
+	DomManager::addCSS('CSS/WeddingParty.css');
 	DomManager::addScript('Scripts/Utils/jquery-1.8.3.min.js');
 ?>
 
@@ -45,15 +46,120 @@
 			<div class="Banner_03  Center">
 				<img src="Files/Images/weddingparty.png" />
 			</div>
+			
+			<div class="WeddingParty">
+				<div class="WeddingPartyRow">
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="Clear"></div>
+				</div>
+				<div class="WeddingPartyCenterRow">
+					<div class="WeddingPartyColumn">
+						<div class="WeddingPartyCell">
+							<div class="CellHover"></div>
+							<img src="http://placekitten.com/100/100">
+						</div>
+						<div class="WeddingPartyCell">
+							<div class="CellHover"></div>
+							<img src="http://placekitten.com/100/100">
+						</div>
+					</div>
+					
+					<div class="WeddingPartyFeature">
+						<img src="http://placekitten.com/204/204">
+					</div>
+					
+					<div class="WeddingPartyColumn">
+						<div class="WeddingPartyCell">
+							<div class="CellHover"></div>
+							<img src="http://placekitten.com/100/100">
+						</div>
+						<div class="WeddingPartyCell">
+							<div class="CellHover"></div>
+							<img src="http://placekitten.com/100/100">
+						</div>
+					</div>
+					<div class="Clear"></div>
+				</div>
+				
+				
+				<div class="WeddingPartyRow">
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="WeddingPartyCell">
+						<div class="CellHover"></div>
+						<img src="http://placekitten.com/100/100">
+					</div>
+					<div class="Clear"></div>
+				</div>
+			</div>
+			
 		</div>
 		<div class="Strip Strip_04">
 			<div class="Banner_04  Center">
 				<img src="Files/Images/theparty.png" />
 			</div>
+			<div class="PartyDetails">
+				<div class="Map">
+					<a href="https://maps.google.com/maps?q=Tralee+Wedding+Facility,+Mountainview+Road,+Caledon,+ON,+Canada&hl=en&sll=37.0625,-95.677068&sspn=42.581364,73.300781&oq=tralee+wedding+facility+ca&hq=Tralee+Wedding+Facility,&hnear=Mountainview+Rd,+Caledon,+Peel+Regional+Municipality,+Ontario,+Canada&t=m&z=12" target="_blank">
+						<img src="Files/Images/map.png" />
+					</a>
+				</div>
+				<div class="Details">
+					<span class="Cursive">Tralee Farms</span></br>
+					CEREMONY AT 2:45 PM</br>
+					DINNER & DANCING TO FOLLOW</br>
+					<div class="Button">
+						<a href="#">
+							MORE EVENT INFO
+						</a>
+					</div></br>
+					<div class="Button">
+						<a href="https://maps.google.com/maps?q=Tralee+Wedding+Facility,+Mountainview+Road,+Caledon,+ON,+Canada&hl=en&sll=37.0625,-95.677068&sspn=42.581364,73.300781&oq=tralee+wedding+facility+ca&hq=Tralee+Wedding+Facility,&hnear=Mountainview+Rd,+Caledon,+Peel+Regional+Municipality,+Ontario,+Canada&t=m&z=12" target="_blank">
+							DRIVING DIRECTIONS
+						</a>
+					</div>
+					
+				</div>
+			</div>
 		</div>
 		<div class="Strip Strip_05">
 			<div class="Banner_05  Center">
 				<img src="Files/Images/wheretostay.png" />
+			</div>
+			<span class="Title">
+				HOPE YOU CAN SHARE IN OUR SPECIAL DAY
+			</span>
+			<div class="InputBox">
+				<div class="Input">
+				</div>
+				<span class="Text">
+					REPLY CODE
+				</span>
 			</div>
 		</div>
 		<div class="Strip Strip_06">
